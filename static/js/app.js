147dc79b80212210a94684659a0bf9efca7b8a35
12wollmana <awollman2@gmail.com>
2020-09-21 19:50:39 -0500

    Finished Level 2

--- static/js/app.js
+++ static/js/app.js
@@ -9,9 +9,23 @@ const tableData = data;
  */
 const elements = 
 {
+    ufoTableBody: d3.select("#ufo-table>tbody"),
+    resetButton: d3.select("#reset-btn"),
     dateOption: d3.select("#datetime"),
-    ufoTableBody: d3.select("#ufo-table>tbody")
+    cityOption: d3.select("#city"),
+    stateOption: d3.select("#state"),
+    countryOption: d3.select("#country"),
+    shapeOption: d3.select("#shape"),
 };
+
+/**
+ * Gets unique values from an array.
+ * From https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+ */
+unique = (array) =>
+{
+    return [...new Set(array)]; 
+}
 
 /**
  * This represents the data model of the table.
@@ -23,38 +37,35 @@ class SightingsTableModel {
      */
     constructor(data){
         this.data = data;
+        this.currentData = data;
     }
 
     /**
      * Gets all the data within the table.
      */
-    getAllData = () => {
-        return data;
-    }
+    getAllData = () => this.data;
 
     /**
-     * Filters a set of data on datetime.
-     * @param {string} datetime 
-     * The datetime to filter on.
-     * @param {any[]} data 
-     * The data to filter on. 
-     * By default, all data in the model.
+     * Sets the current table data.
+     * @param {any[]} data table data
      */
-    getDataByDatetime = (datetime, data = this.data) => {
-        return data.filter((sighting => sighting.datetime === datetime));
-    }
+    setCurrentData = (data) => this.currentData = data;
 
     /**
-     * Returns all unique dates within the data.
-     * @param {any[]} data 
-     * The data to get the datetimes from. 
-     * By default, all data in the model.
+     * Filters a property of the table on a value.
+     * @param {string} filterBy Property to filter.
+     * @param {string} value Value to filter on.
      */
-    getAllDatetimes = (data = this.data) => {
-        const dates = data.map((sighting => sighting.datetime))
-        // Unique dates only
-        // From https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-        return [...new Set(dates)]; 
+    getCurrentDataByFilterValue = (filterBy, value) => 
+        this.currentData.filter(point => point[filterBy] === value);
+    
+    /**
+     * Gets all values under a filterable value.
+     * @param {string} filterBy Which property to get the values of.
+     */
+    getUniqueCurrentDataByFilter = (filterBy) => {
+        const values = this.currentData.map(point => point[filterBy]);
+        return unique(values);
     }
 }
 
@@ -121,52 +132,63 @@ class SightingsTableView {
  * This represents the filter controls in the view.
  * Handles rendering and retrieving options.
  */
-class FilterView 
-{
-    constructor() {}
+class FilterView {
 
     /**
-     * The default option to show.
-     * Should represent showing all data.
+     * This represents a filter control on the view.
+     * @param {d3 element} optionElement The HTML element that corresponds to the filter.
+     * @param {string} defaultValue The default value of the filter. Should represent no filter.
      */
-    defaultDateOption = "All";
-
-    /**
-     * Renders options in the date selection control.
-     * @param {string} dates 
-     * The dates to render as options.
-     */
-    renderDateOptions = (dates) => {
-        this.clearDateOptions();
-        dates.forEach(this.renderDateOption);
+    constructor(optionElement, defaultValue){
+        this.optionElement = optionElement;
+        this.defaultValue = defaultValue;
     }
 
     /**
-     * Renders an option within the date selection control.
-     * @param {string} optionText 
-     * The text to render as an option.
+     * Gets the default value for the filter.
      */
-    renderDateOption = (optionText) => {
-        const options = elements.dateOption;
-        const option = options.append("option");
-        option.text(optionText);
+    getDefaultValue = () => this.defaultValue;
+
+    /**
+     * Gets the HTML element for the filter.
+     */
+    getElement = () => this.optionElement;
+
+    /**
+     * Clears all options but the default.
+     */
+    clearOptions = () => {
+        this.optionElement.html("");
+        this.renderOption(this.defaultValue);
     }
 
     /**
-     * Clears all options from the date selection control 
-     * except the default option.
+     * Gets the currently selected option.
      */
-    clearDateOptions = () => {
-        const options = elements.dateOption;
-        options.html("");
-        this.renderDateOption(this.defaultDateOption);
+    getSelectedOption = () => this.optionElement.property("value");
+
+    /**
+    * Sets the selected option.
+    */
+    setSelectedOption = (optionValue) =>  this.optionElement.property("value", optionValue);
+
+    /**
+    * Renders options in the filter control.
+    * @param {string} optionValues 
+    * The dates to render as options.
+    */
+    renderOptions = (optionValues) => {
+        this.clearOptions();
+        optionValues.forEach(this.renderOption);
     }
 
     /**
-     * Gets the current selection in the date selection control.
+     * Renders a single option in the filter.
+     * @param {string} optionValue The option to render.
      */
-    getSelectedDateOption = () => {
-        return elements.dateOption.property("value");
+    renderOption = (optionValue) => {
+        const option = this.optionElement.append("option");
+        option.text(optionValue);
     }
 }
 
@@ -175,11 +197,24 @@ class FilterView
  */
 class MainController
 {
-    constructor(tableModel, tableView, filterView)
-    {
+    constructor(
+        tableModel, 
+        tableView, 
+        dateFilterView,
+        cityFilterView,
+        stateFilterView,
+        countryFilterView,
+        shapeFilterView
+    ){
         this.tableModel = tableModel;
         this.tableView = tableView;
-        this.filterView = filterView;
+        this.filterViews = {
+            datetime : dateFilterView,
+            city : cityFilterView,
+            state : stateFilterView,
+            country : countryFilterView,
+            shape : shapeFilterView
+        }
     }
 
     /**
@@ -190,28 +225,57 @@ class MainController
     {
         const tableData = this.tableModel.getAllData();
         this.tableView.renderTable(tableData);
-
-        const tableDates = this.tableModel.getAllDatetimes();
-        this.filterView.renderDateOptions(tableDates);
-
-        elements.dateOption.on("change", this.filterByDate);
+        Object.entries(this.filterViews).map(([filter, view])=> {
+            const tableValues = this.tableModel.getUniqueCurrentDataByFilter(filter);
+            view.renderOptions(tableValues);
+            view.getElement().on("change", () => this.filter());
+        });
+        elements.resetButton.on("click", () => this.resetFilters());
     }
 
     /**
-     * Filters the data by the selection in the 
-     * date filter control.
+     * Reloads the options for a filter.
+     * @param {string} filter The filter property.
+     * @param {FilterView} view The filter view.
      */
-    filterByDate = () =>
-    {
-        const selection = this.filterView.getSelectedDateOption();
-        let filteredData = [];
-        if(selection === this.filterView.defaultDateOption){
-            filteredData = this.tableModel.getAllData();
-        }
-        else {
-            filteredData = this.tableModel.getDataByDatetime(selection);
-        }
+    reloadFilterOptions = (filter, view) =>{
+        const selection = view.getSelectedOption();
+        const tableValues = this.tableModel.getUniqueCurrentDataByFilter(filter);
+        view.renderOptions(tableValues);
+        view.setSelectedOption(selection);
+    }
+
+    /**
+     * Applies all filters to the table.
+     */
+    filter = () => {
+        // Filter the data
+        let filteredData = this.tableModel.getAllData();
+        Object.entries(this.filterViews).map(([filter, view])=> {
+            const selection = view.getSelectedOption();
+            if(selection !== view.getDefaultValue()){
+                filteredData = filteredData.filter(point => point[filter] === selection);
+            }
+        });
+
+        // Render Data
         this.tableView.renderTable(filteredData);
+        this.tableModel.setCurrentData(filteredData);
+
+        // Filter the options
+        Object.entries(this.filterViews).map(([filter, view])=> {
+            this.reloadFilterOptions(filter, view)
+        });
+    }
+
+    /**
+     * Resets all filters on the table.
+     */
+    resetFilters = () => {
+        Object.entries(this.filterViews).map(([filter, view])=> {
+            view.setSelectedOption(view.getDefaultValue());
+        })
+        this.filter();
     }
 }
 
@@ -223,7 +287,11 @@ class MainController
     let controller = new MainController(
         new SightingsTableModel(tableData),
         new SightingsTableView(),
-        new FilterView()
+        new FilterView(elements.dateOption, "All"),
+        new FilterView(elements.cityOption, "All"),
+        new FilterView(elements.stateOption, "All"),
+        new FilterView(elements.countryOption, "All"),
+        new FilterView(elements.shapeOption, "All")
     );
     controller.init();
 })()
